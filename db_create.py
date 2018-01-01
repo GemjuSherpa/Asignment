ad8a9ecf3bd8647e5746eb56760a4c6c4ce04951
gemju <gemju@github.com>
2018-01-02 00:02:48 +1000

    eddited db_create.py

--- db_create.py
+++ db_create.py
@@ -31,15 +31,28 @@ def create_table():
 		'Latitude    VARCHAR(20),'
 		'Longitude   VARCHAR(20));'
 	)
-create_table()
+#create_table()
 
 def load_workbook(filename, sheet):
-	wb = openpyxl.load_workbook("filename")
-	sheet = wb.get_sheet_by_name('sheet')
+	wb = openpyxl.load_workbook(filename)
+	sheet = wb.get_sheet_by_name(sheet)
 
-	return sheet
+	rows_iter = sheet.iter_rows(min_col=0, min_row=1)
+	vals = [[cell.value for cell in row] for row in rows_iter]
+	datalists = []
+	datalists.append(vals)
 
-load_workbook('GlobalLandTemperaturesByCountry.xlsx', 'GlobalLandTemperaturesByCountry')
+	return datalists
 
-def insert_data():
-	return True
+#load_workbook('GlobalLandTemperaturesByCountry.xlsx', 'GlobalLandTemperaturesByCountry')
+#load_workbook('GlobalLandTemperaturesByState.xlsx', 'GlobalLandTemperaturesByState')
+
+
+data = load_workbook('GlobalLandTemperaturesByCountry.xlsx', 'GlobalLandTemperaturesByCountry')
+for p in data:
+	format_str = """INSERT INTO Bycountry
+	VALUES ("{Date}", "{Average_Temp}", "{Avg_Temp_Uncertainty}", "{Country}");"""
+	sql_command = format_str.format(Date=p[0], Average_Temp=p[1], Avg_Temp_Uncertainty=p[2], Country=p[3])
+	cursor.execute(sql_command)
+
+
