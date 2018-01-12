2a7f6af596ee95215695e98f80c1f4a675f5b595
gemju <gemju@github.com>
2018-01-12 23:32:34 +1000

    numpy_temp.py finished

--- numpy_temp.py
+++ numpy_temp.py
@@ -1,14 +1,22 @@
+# #!/bin/Python
+# Filename: numpy_temp.py
+# Author: Gemju Sherpa
+
+
 import openpyxl
 import numpy as np
 import matplotlib.pyplot as plt
 import sqlite3
 
+# Open workbook and create a new sheet comparision
 wb = openpyxl.load_workbook('World Temperature.xlsx')
 sheet = wb.create_sheet('Comparision')
 
+# Connect to db
 connection = sqlite3.connect("globaltemperature.db")
 cursor = connection.cursor()
 
+# Sqlite statement to get state data
 sql_state = """
 	select State, strftime('%Y', Date), Average_Temp
 	from Bystate where Country = 'Australia' 
@@ -16,6 +24,7 @@ sql_state = """
 
 """
 
+#Sqlite statement to get country data
 sql_national = """
 	select strftime('%Y', Date), Average_Temp
 	from Bycountry where Country = 'Australia' 
@@ -23,15 +32,16 @@ sql_national = """
 
 """
 
-def stateData(sql_state):
-	cursor.execute(sql_state)
+
+## Function to calculate the mean Temp for each year of state
+def state_data(sql):
+	cursor.execute(sql)
 	array1 = np.array(cursor.fetchall())
 
 	year_temp = array1[:, 1::]
 
 	year = []
 	temp = []
-
 	for i in year_temp:
 		year.append(i[0])
 		temp.append(i[1])
@@ -57,19 +67,20 @@ def stateData(sql_state):
 	for value in valuesfloat:
 		mean.append(sum(value)/len(valuesfloat)) # calculate the mean temp for each year
 	mean2 = list(zip(keys, mean))
-	return mean2
-####################################
-def nationalData(sql_national):
-	cursor.execute(sql_national)
+	return mean2 # Return mean value
+
+
+## Function to calculate the mean Temp for each year of Country Australia
+def national_data(sql):
+	cursor.execute(sql)
 	array2 = np.array(cursor.fetchall())
 	year1 = []
 	temp1 = []
-
 	for i in array2:
 		year1.append(i[0])
 		temp1.append(i[1])
 
-	data1 = list(zip(year1, temp1)) # create a dictionaries k, v -> year, temp
+	data1 = list(zip(year1, temp1))# create a dictionaries k, v -> year, temp
 
 	d1 = {}
 	for k, v in data1:
@@ -81,31 +92,40 @@ def nationalData(sql_national):
 		values1.append(v)
 		keys1.append(k)
 
-	valuesfloat1 = [] # lists of values converted to floats
+	valuesfloat1 = []# lists of values converted to floats
 	mean1 = []
 	for j in values1:
 		valuesfloat1.append([float(i) for i in j if i != "None"]) # eliminate the None values
 	for value in valuesfloat1:
 		mean1.append(sum(value)/len(valuesfloat1)) # calculate the mean temp for each year
 	mean3 = list(zip(keys1, mean1))
-	return mean3
-#######
+	return mean3 # return mean value
 
 
-m1 = stateData(sql_state)
-npm1 = np.array(m1)
-x1 = npm1[:, 0]
-y1 = npm1[:, 1]
+#extract the year and mean data for ploting
+m1 = np.array(state_data(sql_state)) #mean data state data
+x1 = m1[:, 0]
+y1 = m1[:, 1]
 
-m2 = nationalData(sql_national)
-npm2 = np.array(m2)
 
-x2 = npm2[:, 0]
-y2 = npm2[:, 1]
+m2 = np.array(national_data(sql_national)) # mean data for national data
+x2 = m2[:, 0]
+y2 = m2[:, 1]
 
-plt.plot(x1, y1)
-plt.plot(x2, y2)
-plt.show()
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.plot(x1, y1) # ploting state data
+
+ax.plot(x2, y2) # ploting country data
+plt.savefig('plt.png') # save as png image
+
+img = openpyxl.drawing.image.Image('plt.png') # draw image to sheet
+img.anchor(sheet['B2'])
+
+sheet.add_image(img)
+wb.save('World Temperature.xlsx') # save file
+wb.close() # close workbook
 
 
 
